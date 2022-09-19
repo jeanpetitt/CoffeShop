0c73cfae7b1675d958a2f82bb24dcac8a7792ac2
jeanpetit <jeanpetitt>
2022-09-20 00:38:29 +0100

    configurer le frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'fullstack-udacity-ndg.us.auth0.com', 
+    audience: 'coffeshop', 
+    clientId: 'i15mgsT1JwHJ1pbnZGxG4Sxs1EYL1a0W',
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
